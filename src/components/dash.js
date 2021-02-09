464a586d4b28b8cdc3d8491998e4e05a192759bd
Barinder Preet <54902053+captain-bari@users.noreply.github.com>
2021-02-09 15:33:12 +0530

    login

---
+++ src/components/dash.js
@@ -0,0 +1,15 @@
+import React, {components, useEffect,useState} from "react";
+
+const Dash=()=> 
+{ 
+useEffect(() => {}, [])
+
+
+  return <div>DASH
+
+
+
+  </div>; 
+} 
+  
+export default Dash;
